aa0ee58ed83852f88c6412a11ca66f73545d58d9
Fikerte-T <tofikertesfaye@gmail.com>
2022-06-07 15:59:46 +0300

    Create thunk and slice for cars

---
+++ src/store/car.js
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+// create thunk
+export const getCars = createAsyncThunk('cars/getCars', 
+    async() => {
+        return fetch('http://localhost:3001/api/v1/cars').then((res) =>
+        res.json()
+    )}
+)
+
+// create slice
+export const carsSlice = createSlice({
+    name: 'cars',
+    initialState: {
+        carList: [],
+        status: null,
+    },
+    extraReducers: {
+        [getCars.pending]: (state) => {
+            state.status = 'pending'
+        },
+        [getCars.fullfilled]: (state, action) => {
+            state.carList = payload
+            state.status = 'success'
+        },
+        [getCars.rejected]: (state) => {
+            state.status = 'failed'
+        }
+    }
+})
+
+export default carsSlice.reducer;
