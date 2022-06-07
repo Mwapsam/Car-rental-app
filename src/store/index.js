d5cb707c047240594848f816ba07ef9a6e22ed2e
Fikerte-T <tofikertesfaye@gmail.com>
2022-06-07 16:03:31 +0300

    Add cars to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import user from './user';
+import carsReducer from './car/carsSlice'
 
 const reducer = combineReducers({
   user,
+  cars: carsReducer
 });
 const store = configureStore({
   reducer,
