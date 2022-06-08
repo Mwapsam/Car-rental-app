73ac2951f5b1f57479f984cddc58d361b916ccc5
Fatima Ezzahra <felemenoun@gmail.com>
2022-06-08 21:38:53 +0100

    Setup Car detail component

--- src/pages/car/Car.js
+++ src/pages/car/Car.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-const Car = () => <div>Show single Car</div>;
+const Car = () => {
+	
+	return (
+	<div>Show single Car</div>
+	)};
 
 export default Car;
