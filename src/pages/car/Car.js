4a556a15a9f059b2ba6a96e80027ff0ea60ff9c4
Fatima Ezzahra <felemenoun@gmail.com>
2022-06-08 21:55:09 +0100

    Get the car details and filter the data

--- src/pages/car/Car.js
+++ src/pages/car/Car.js
@@ -1,8 +1,19 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import getCars from '../../services/car.service';
 
 const Car = () => {
+  const { carsList } = useSelector((state) => state.cars);
+  const dispatch = useDispatch();
+  const { carId } = useParams();
+	const carDetail = carsList.filter((car) => car.id === +carId)
+
+	useEffect(() => {
+    dispatch(getCars());
+  }, []);
 	
+
 	return (
 	<div>Show single Car</div>
 	)};
