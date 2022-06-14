02cc1848083ecca50adabc64a3d84c633859f421
Fatima Ezzahra <felemenoun@gmail.com>
2022-06-14 22:01:40 +0100

    Add test for AddCar component

--- src/__tests__/components/reservations/Car.test.js
+++ src/__tests__/components/reservations/Car.test.js
@@ -3,11 +3,21 @@ import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import store from '../../../store/index';
 import CarsList from '../../../pages/car/CarsList';
+import AddCar from '../../../pages/car/AddCar';
 
 it('should render Cars List component', () => {
 const tree = renderer.create(
 	<Provider store={store}>
 		<CarsList />
+	</Provider>
+)
+expect(tree).toMatchSnapshot();
+});
+
+it('should render AddCar component', () => {
+const tree = renderer.create(
+	<Provider store={store}>
+		<AddCar />
 	</Provider>
 )
 expect(tree).toMatchSnapshot();
