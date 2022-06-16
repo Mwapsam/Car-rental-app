5038a259d04bbbc46c7539f10e2cc3bdf21b0813
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-16 11:25:33 +0200

    Configure for production

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 const UserService = {
   signup: (formData) => axios.post(`${process.env.REACT_APP_SERVER}signup`, {
     user: formData,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
   }),
   login: (formData) => axios.post(`${process.env.REACT_APP_SERVER}login`, formData),
   logout: () => {},
